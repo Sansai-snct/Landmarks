b7ed5605fc8e1fbc6378827f2866901f3dc945ab
sansai-SNCT <syunzo1115@icloud.com>
2022-05-22 23:31:01 +0900

    [Add] Create Image view file

---
+++ Landmarks/CicleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CicleImage.swift
+//  Landmarks
+//
+//  Created by 山際駿太 on 2022/05/22.
+//
+
+import SwiftUI
+ 
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+ 
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
