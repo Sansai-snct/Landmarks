d028892362324ec46a3355ddc963fd1a85fbec45
sansai-SNCT <syunzo1115@icloud.com>
2022-05-22 23:37:45 +0900

    [Add] Create MapView

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,16 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 山際駿太 on 2022/05/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    var body: some View {
+        Text("Hello")
+    }
+}
