e2c988338da65d2cbe355e97066cf9383b7f907c
sansai-SNCT <syunzo1115@icloud.com>
2022-05-22 23:18:06 +0900

    [Update] Add  Vertical Stack

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Text("Turtle Rock")
-            .font(.title)
+        VStack{
+            Text("Turtle Rock")
+                .font(.title)
+            
+        }
+       
     }
 }
 
