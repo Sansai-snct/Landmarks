e43c76b7cfa59e16db0627108cfb37f1ff33087e
sansai-SNCT <syunzo1115@icloud.com>
2022-05-22 23:16:25 +0900

    [Update] Change text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI!")
-            .font(.title)
-            .foregroundColor(.green)
-            .fontWeight(.heavy)
+       Text("Turtle Rock")
+            
     }
 }
 
